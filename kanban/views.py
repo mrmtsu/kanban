18b0b4c6ab430654609d8d00f5bdf3c246bd9d7e
mrmtsu <81202muramatsu@gmail.com>
2020-04-17 16:27:39 +0900

    login_new

--- kanban/views.py
+++ kanban/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,3 +9,21 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "kanban/index.html")
+
+def home(request):
+    return render(request, "kanban/home.html")
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user_instance = form.save()
+            login(request, user_instance)
+            return redirect("kanban:home")
+    else:
+        form = UserCreationForm()
+
+    context = {
+        "form": form
+    }
+    return render(request, 'kanban/signup.html', context)
